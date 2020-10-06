de5866e5f0b8391a567c622b0dc2b0461a111ab7
Basma Nizar Mohammed Eid <basmanizar@outlook.com>
2020-10-07 02:12:06 +0300

    Lab-37

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+# React 1 - Odometer
+
+## Overview
+
+Create an Odometer web page using ReactJS.
+
+## Feature Tasks and Requirements
+
+- Display an odometer with 4 digits
+  - Pad with zeroes as needed (e.g. 0045)
+    - JavaScript has a method for this. Research as needed.
+- Add buttons to increment the “miles” by 1, 10, 100 and 1000.
+  - Roll over when odometer value exceeds 9999
+  -Maintain any “extra” amount when rolling over.
+- The root ```App``` component should contain ```Odometer```, ```Header``` and ```Footer``` components.
+  - You decide when appropriate to use class based or functional components.
+  - ```Header``` component should receive a ```text``` value via props.
+  - ```Footer``` component should receive a ```trademark``` value via props.
+  - ```Odometer``` component should store numeric value in it’s state.
+    - Remember to update state in the React way.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App">
+      <div className="AppTopic">
+        <h1 className="AppHeader">{props.text}</h1>
+      </div>
+    </header>
   );
 }
 
-export default App;
+function Main() {
+  return (
+    <main className="AppMain">
+      <Odometer />
+    </main>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer className="AppFooter">
+      <p className="AppParaFooter">{props.trademark} &sdot; All rights are reserved &copy; {props.year}</p>
+    </footer>
+  );
+}
+
+class Odometer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      odometer: "0000"
+    };
+  }
+  add(num) {
+    this.state.counter += num
+    if (this.state.counter > 9999) {
+      this.state.counter = this.state.counter - 9999
+    }
+    let setOtometer = this.state.counter.toString().padStart(4, "0");
+    this.setState({
+      counter: this.state.counter,
+      odometer: setOtometer
+    });
+  }
+  render() {
+    return (
+      <>
+        <h1 className="AppOdometer">{this.state.odometer} miles</h1>
+        <div className="AppButtons">
+          <button onClick={() => this.add(1)} className="AppButton">Add 1 mile</button>
+          <button onClick={() => this.add(10)} className="AppButton">Add 10 miles</button>
+          <button onClick={() => this.add(100)} className="AppButton">Add 100 miles</button>
+          <button onClick={() => this.add(1000)} className="AppButton">Add 1000 miles</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default function App() {
+  return (
+    <>
+      <Header text="Odometer" />
+      <Main />
+      <Footer trademark="ASAC" year="2020" />
+    </>
+  );
+}
